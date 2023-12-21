3a48d7f388131d4a428b36e4fddbd69060d653b1
Luqman Butt <luqmanmbutt.dev@gmail.com>
2023-12-21 15:49:25 +0000

    added container for to-do-list

--- to-to-list/src/App.js
+++ to-to-list/src/App.js
@@ -1,12 +1,36 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className='main-container'>
-      HEllO
+      <div className='first-container'> 
+        <div className='to-do-list-title'>
+          TO-DO-LIST
+        </div>
+        <div className='second-container'>
+          
+          <div className='add-to-list-container'>
+             
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+/**
+ * Two things a function can do 
+ * 1 - Does not return anything but, Executes lines that are inside the function 
+ * 2 - Returns something for us to use 
+ */
+const randomFunction = () => {
+
+console.log("randomFunction -- works")
+
+}
+
+randomFunction()
